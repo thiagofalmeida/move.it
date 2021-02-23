38b671d3ddc5fbdbf79b6ff936c1231e8780ab59
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2021-02-23 11:08:33 -0300

    Day 2

---
+++ src/components/Profile.tsx
@@ -0,0 +1,18 @@
+import styles from '../styles/components/Profile.module.css'
+
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/thiagofalmeida.png" alt="Thiago"/>
+
+      <div>
+        <strong>Thiago Almeida</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level"/>
+          Level 1
+        </p>
+      </div>
+    </div>
+  )
+}
